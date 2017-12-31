0068743267926ad6946085204cafe71c05b5122e
Juurinen Juho T <juho.juurinen@gmail.com>
2017-12-31 23:44:57 +0200

    facelift : hello bootstrap

--- src/main/java/vouchertracker/controller/DefaultController.java
+++ src/main/java/vouchertracker/controller/DefaultController.java
@@ -3,15 +3,13 @@ package vouchertracker.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class DefaultController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    @ResponseBody
     public String hello() {
-        return "Hello world!!";
+        return "vouchers";
     }
 
     @RequestMapping("*")
